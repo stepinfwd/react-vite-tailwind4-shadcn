b8032fe0b2fc2a698642e5d6d088d4ea24d90419
stepinfwd <stepinfwd@gmail.com>
2025-03-24 17:48:50 +0530

    Initial commit for starter template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Button } from './components/ui/button';
+function App() {
+  return (
+    <>
+      <div className="flex flex-col">
+        <p className="text-lg font-sans">
+          Complete Tailwind CSS v4 + Vite Setup (JavaScript/React) + ShadCn
+        </p>
+        <Button className="">ShadCn</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
