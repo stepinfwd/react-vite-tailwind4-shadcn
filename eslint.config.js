0461f65e8974ca6a77af53ac3a3704df386272a3
stepinfwd <stepinfwd@gmail.com>
2025-03-26 12:54:38 +0530

    chore : lint fix for throwing error on not importing react

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ export default [
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules, 
       ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
