84f92347ed924b35cb2aa5a3778ef94d2f456c96
stepinfwd <stepinfwd@gmail.com>
2025-03-26 12:35:40 +0530

    chore : es lint not throwing error when not imported file is used

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react' // Add this for throwing error on component if not imported
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
@@ -17,11 +18,13 @@ export default [
       },
     },
     plugins: {
+      'react': react, 
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules, 
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
